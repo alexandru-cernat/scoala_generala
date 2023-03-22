1ae86de20f60df5d663ee09685790631c8690fae
alexandru-cernat <a-i.cernat@metro.digital>
2023-03-22 11:40:18 +0200

    init

---
+++ src/main/java/com/example/scoala_generala/ScoalaGeneralaApplication.java
@@ -0,0 +1,13 @@
+package com.example.scoala_generala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoalaGeneralaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoalaGeneralaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scoala_generala/ScoalaGeneralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scoala_generala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoalaGeneralaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
